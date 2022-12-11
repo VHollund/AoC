368397545313439d768ecfc056f67fb9bdabb393
Vetle Ørstavik Hollund <vetleh15@uia.no>
2022-12-11 22:50:05 +0100

    :sparkles: Day11 part2

--- Y2022/Day11/main.py
+++ Y2022/Day11/main.py
@@ -1,44 +1,44 @@
+import operator
 import typing
 from functools import reduce
 
 from Helpers.GetInput import *
 
+ops = {
+    '+': operator.add,
+    '*': operator.mul,
+}
 
 class monkey():
-    def __init__(self, id, items, divNumber, OM, operator, opNum, true, false):
-        self.id:int = id
-        self.items: typing.List[int] = []
+    def __init__(self, number, items, divNumber, OM, operator, opNum, true, false):
+        self.id: int = number
+        self.items: typing.List[int] = items
         self.otherMonkeys: typing.List[monkey] = OM
-        self.test:int = divNumber
-        self.operator:str = operator
-        self.opNum: int = opNum
+        self.test: int = divNumber
+        self.operator: str = operator
+        self.opNum = opNum
+        self.multiplier = opNum
         self.sendIfFalse = false
         self.sendIftrue = true
-        self.newitems: typing.List[int] = items
         self.inspectedItem = 0
 
     def run(self, worrydivider=3):
-        self.items = self.newitems
-        self.newitems = []
         for x in range(len(self.items)):
             item = self.items[x]
-            if self.operator == "*":
-                item *= self.opNum
-            elif self.operator == "**":
-                item = item ** self.opNum
+            if self.opNum == "old":
+                self.multiplier = item
+            newItem=ops[self.operator](item, int(self.multiplier))
+            if worrydivider == 3:
+                newItem //= worrydivider
             else:
-                item += self.opNum
-            if worrydivider==3:
-                item //= worrydivider
-            else:
-                item %= worrydivider
+                newItem %= worrydivider
             self.inspectedItem += 1
-            otherMonkey = self.otherMonkeys[self.sendIftrue if item % self.test == 0 else self.sendIfFalse]
-            otherMonkey.newitems.append(item)
+            otherMonkey = self.otherMonkeys[self.sendIftrue if newItem % self.test == 0 else self.sendIfFalse]
+            otherMonkey.items.append(newItem)
         self.items = []
 
     def __str__(self):
-        return f"Monkey {self.id}: {self.newitems} and inspected items {self.inspectedItem} times."
+        return f"Monkey {self.id}: {self.items} and inspected items {self.inspectedItem} times."
 
 
 def chunks(l, n):
@@ -55,12 +55,10 @@ def parseMonkeys(data) -> typing.List[monkey]:
             d0 = int(y[0].split(" ")[1][0:1])
             d1 = [int(l) for l in y[1].split(": ")[1].split(",")]
             d2 = y[2].split("new = old ")[1].split(" ")
-            if d2 == ["*", "old"]:
-                d2 = ["**", 2]
             d3 = int(y[3].split("divisible by ")[1])
             d4 = int(y[4].split("throw to monkey ")[1])
             d5 = int(y[5].split("throw to monkey ")[1])
-            monkeylist.append(monkey(d0, d1, d3, monkeylist, d2[0], int(d2[1]), d4, d5))
+            monkeylist.append(monkey(d0, d1, d3, monkeylist, d2[0], d2[1], d4, d5))
     return monkeylist
 #(id, items, divNumber, OM, operator, opNum, true, false):
 
@@ -80,8 +78,8 @@ def part1(data):
 def part2(data):
     monkeylist: typing.List[monkey] = parseMonkeys(data)
     divisorLimit=reduce(lambda c, m: c * m.test, monkeylist, 1)
+    print(divisorLimit)
     for x in range(10000):
-        print(x)
         for z in monkeylist:
             z.run(divisorLimit)
     for x in monkeylist:
@@ -95,5 +93,8 @@ def Day11():
     part2("Y2022/Day11/input.txt")
 
 
+#9699690
+#78678
+#14416204136
 if __name__ == "__main__":
     Day11()
